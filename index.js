6223d0aed83e4e3570548ebdc9f663f4853af220
Santi Herrero <santihbc@gmail.com>
2016-05-24 12:26:43 +0200

    Separate routes in Routers

--- index.js
+++ index.js
@@ -1,49 +1,16 @@
-// Basic Express.js server
+// Basic Usage of Routers in Express.js
 
 var express = require("express");
 var bodyParser = require("body-parser");
+var todosRoutes = require("./api/todos_routes");
 var app = express();
-
-var todos = [
-	"Check out EDpuzzle",
-	"Grab a free beer from the fridge",
-	"Stop using Todos as an example app"
-];
-
-// Middleware to parse the info passed in the request
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
-/* ====================================================== */
-/*                      Middleware                        */
-/* ====================================================== */
-
-function requestLogger (req, res, next) {
-	console.log(">>>> Request received! <<<<");
-	return next();
-}
-
-function errorHandler (err, req, res, next) {
-	console.log("💣💥 KABOOM!! (" + err.message + ") 💥💣");
-	return res.status(500).json();
-}
 
 /* ====================================================== */
 /*                        Routes                          */
 /* ====================================================== */
 
-// GET
-// ------
-
-// curl -X GET http://localhost:3000/todos
-app.get("/todos", requestLogger, function (req, res) {
-
-	throw new Error("Horrible error");
-
-	res.status(200).json(todos);
-});
-
-app.use(errorHandler);
+app.use("/api/v1/todos", todosRoutes);
+app.use("/api/v2/todos", todosRoutes);
 
 // Start Server on port 3000
 app.listen(3000);
